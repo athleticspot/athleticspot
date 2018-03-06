963262a83eaa1a2f6a0a6d3a1e55e97548c355c8
tomkasp@gmail.com <tomkasp@gmail.com>
2018-03-06 21:01:01 +0100

    module change + tracker initialization

---
+++ tracker/src/main/java/com/athleticspot/tracker/infrastracture/web/ApiUrl.java
@@ -0,0 +1,11 @@
+package com.athleticspot.tracker.infrastracture.web;
+
+/**
+ * @author Tomasz Kasprzycki
+ */
+public class ApiUrl {
+
+    public static final String API = "/api";
+    public static final String TRACKER_URL = API + "/tracker";
+
+}
---
+++ tracker/src/main/java/com/athleticspot/tracker/infrastracture/web/TrackerController.java
@@ -0,0 +1,18 @@
+package com.athleticspot.tracker.infrastracture.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Tomasz Kasprzycki
+ */
+@RestController
+@RequestMapping(value = ApiUrl.TRACKER_URL)
+public class TrackerController {
+
+    @GetMapping
+    public String getData() {
+        return "hello from tracker";
+    }
+}
