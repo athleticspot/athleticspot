737dd481e3681664dde1ccbc2b685b1ec85e702d
tomkasp@gmail.com <tomkasp@gmail.com>
2018-08-05 21:18:48 +0200

    controller placeholders

---
+++ tracker/src/main/java/com/athleticspot/tracker/infrastracture/web/TimelineController.java
@@ -0,0 +1,12 @@
+package com.athleticspot.tracker.infrastracture.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Tomasz Kasprzycki
+ */
+@RestController
+@RequestMapping
+public class TimelineController {
+}
