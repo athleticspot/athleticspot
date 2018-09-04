090c883bb2959efa411096ca7619401adbee2a6b
tomkasp@gmail.com <tomkasp@gmail.com>
2018-09-04 20:54:26 +0200

    add activity form

--- web/src/main/webapp/app/timeline/activities/activities.component.ts
+++ web/src/main/webapp/app/timeline/activities/activities.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import {ActivitiesDataservice} from "./activities.dataservice";
+import {FormControl, FormGroup} from "@angular/forms";
+import {ActivityModel} from "./activity.model";
 
 @Component({
     selector: 'athleticspot-activities',
@@ -7,14 +9,46 @@ import {ActivitiesDataservice} from "./activities.dataservice";
 })
 export class ActivitiesComponent implements OnInit {
 
+    private addActivityForm: FormGroup;
+
+
     constructor(private activityDataservice: ActivitiesDataservice) {
     }
 
     ngOnInit(): void {
-        this.activityDataservice.fetchActivity().subscribe(value =>
-            console.log(value)
-        );
-        console.log("Athleticspot activities list")
+        this.addActivityForm = new FormGroup({
+            'title': new FormControl(),
+            'description': new FormControl(),
+            'type': new FormControl(),
+            'duration': new FormControl(),
+            'distance': new FormControl(),
+            'maxSpeed': new FormControl(),
+            'meanSpeed': new FormControl()
+        });
+        // this.activityDataservice.fetchActivity().subscribe(value =>
+        //     console.log(value)
+        // );
     }
 
+    submitActivity() {
+        let activity = this.addActivityForm.value as ActivityModel;
+        console.log(activity);
+        this.activityDataservice.createActivity(activity).subscribe(isSuccess => {
+            console.log(isSuccess);
+        }, error => {
+            console.log(error);
+        });
+    }
 }
+
+//
+// public _source = "MANUAL",
+//     public _title = "title",
+//     public _description = "description",
+//     public _type = "running",
+//     public _duration = "25",
+//     public _distance = "10",
+//     public _units = "km",
+//     public _maxSpeed = "33",
+//     public _meanSpeed = "15",
+//     public _dateTime = new Date())
