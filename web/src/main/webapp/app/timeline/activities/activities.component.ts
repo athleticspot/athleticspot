9af5927100b09e416c982e003328229519f8349a
tomkasp@gmail.com <tomkasp@gmail.com>
2018-09-06 11:11:50 +0200

    duration widget

--- web/src/main/webapp/app/timeline/activities/activities.component.ts
+++ web/src/main/webapp/app/timeline/activities/activities.component.ts
@@ -20,7 +20,11 @@ export class ActivitiesComponent implements OnInit {
             'title': new FormControl(null, Validators.required),
             'description': new FormControl(),
             'type': new FormControl("Running", Validators.required),
-            'duration': new FormControl(null, Validators.required),
+            'duration': new FormGroup({
+                "hours": new FormControl(0),
+                "minutes": new FormControl(0),
+                "seconds": new FormControl(0)
+            }),
             'distance': new FormControl(),
             'maxSpeed': new FormControl(),
             'meanSpeed': new FormControl()
@@ -35,6 +39,9 @@ export class ActivitiesComponent implements OnInit {
         activity.source = "MANUAL";
         activity.dateTime = new Date();
         activity.units = 'metric';
+        activity.duration = this.addActivityForm.get("duration.hours").value + "," +
+            this.addActivityForm.get("duration.minutes").value + "," +
+            this.addActivityForm.get("duration.seconds").value;
         console.log(activity);
         this.activityDataservice.createActivity(activity).subscribe(isSuccess => {
             console.log(isSuccess);
