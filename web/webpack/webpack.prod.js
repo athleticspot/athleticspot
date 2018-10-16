f5de633d644d226eb2c5eb30ff61c9e061188004
tomkasp@gmail.com <tomkasp@gmail.com>
2018-10-16 21:26:18 +0200

    postgress + webpack configuration

--- web/webpack/webpack.prod.js
+++ web/webpack/webpack.prod.js
@@ -1,25 +1,36 @@
+const webpack = require('webpack');
+const writeFilePlugin = require('write-file-webpack-plugin');
 const webpackMerge = require('webpack-merge');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const Visualizer = require('webpack-visualizer-plugin');
+const execSync = require('child_process').execSync;
+const fs = require('fs');
 const path = require('path');
 
 const commonConfig = require('./webpack.common.js');
 
 const ENV = 'prod';
 
+const ddlPath = './target/www/vendor.json';
+
+if (!fs.existsSync(ddlPath)) {
+    execSync('webpack --config webpack/webpack.vendor.js');
+}
+
 module.exports = webpackMerge(commonConfig({env: ENV}), {
     devtool: 'source-map',
     output: {
         path: path.resolve('./target/www'),
-        filename: 'app/[hash].[name].bundle.js',
-        chunkFilename: 'app/[hash].[id].chunk.js'
+        filename: 'app/[name].bundle.js',
+        chunkFilename: 'app/[id].chunk.js'
     },
     plugins: [
-        new ExtractTextPlugin('[hash].styles.css'),
-        new Visualizer({
-            // Webpack statistics in target folder
-            filename: '../stats.html'
-        })
+        new ExtractTextPlugin('styles.css'),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new writeFilePlugin(),
+        new webpack.WatchIgnorePlugin([
+            path.resolve('./src/test'),
+        ])
     ]
 });
