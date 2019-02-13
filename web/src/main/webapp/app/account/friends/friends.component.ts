e038e223c28d099d486efda1355c39be41616522
tomkasp@gmail.com <tomkasp@gmail.com>
2019-02-13 20:27:07 +0100

    Friends component

--- web/src/main/webapp/app/account/friends/friends.component.ts
+++ web/src/main/webapp/app/account/friends/friends.component.ts
@@ -8,21 +8,26 @@ import {FormControl, FormGroup} from "@angular/forms";
 export class FriendsComponent implements OnInit {
 
     private searchFriendsForm: FormGroup;
+    private friends = [];
 
     constructor(private friendsDataservice: FriendsDataservice) {
     }
 
     ngOnInit() {
         this.searchFriendsForm = new FormGroup({
-            "name": new FormControl(null)
+            "name": new FormControl("")
         });
     }
 
     public searchFriends() {
+        this.friends = [];
         let name = this.searchFriendsForm.controls.name.value;
         this.friendsDataservice.fetchFriendsPaged(0, name).subscribe(
             (friendsPage: any) => {
                 console.log(friendsPage);
+                friendsPage.content.forEach(friend => {
+                    this.friends.push(friend)
+                });
             }, error => {
 
             }
