5d8299efaae23cd1a6829d06dae3e1af59483b9d
tomkasp@gmail.com <tomkasp@gmail.com>
2019-04-19 22:13:52 +0200

    adding mailgun support

--- web/src/main/java/com/athleticspot/service/ProdMailService.java
+++ web/src/main/java/com/athleticspot/service/ProdMailService.java
@@ -1,31 +1,35 @@
 package com.athleticspot.service;
 
 import com.athleticspot.domain.User;
-import com.sendgrid.*;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 import io.github.jhipster.config.JHipsterConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import javax.ws.rs.core.MediaType;
 
 /**
  * @author Tomasz Kasprzycki
  */
 @Service
 @Profile({JHipsterConstants.SPRING_PROFILE_PRODUCTION})
-public class ProdMailService implements MailService{
+public class ProdMailService implements MailService {
 
     private final Logger log = LoggerFactory.getLogger(ProdMailService.class);
 
-    private final SendGrid sendGrid;
 
-    @Autowired
-    public ProdMailService(SendGrid sendGrid) {
-        this.sendGrid = sendGrid;
-    }
+    @Value("${mailgun.api-key}")
+    private String mailgunApiKey;
+
+    @Value("${mailgun.domain}")
+    private String domain;
 
     @Override
     public void sendEmail(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
@@ -34,24 +38,18 @@ public class ProdMailService implements MailService{
 
     @Override
     public void sendEmail(String toString, String subject, String contentString) {
-        log.info("Reseting password");
-        Email from = new Email("test@example.com");
-        Email to = new Email(toString);
-        Content content = new Content("text/plain", contentString);
-        Mail mail = new Mail(from, subject, to, content);
-
-        Request request = new Request();
-        try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-            Response response = this.sendGrid.api(request);
-            sendGrid.api(request);
-
-        } catch (IOException ex) {
-            log.error("Error occured during email send", ex);
-        }
-
+//
+//        Configuration configuration = new Configuration()
+//            .domain(domain)
+//            .apiKey(mailgunApiKey)
+//            .from("Test account", "postmaster@somedomain.com");
+//
+//        Mail.using(configuration)
+//            .to("marty@mcfly.com")
+//            .subject("This is the subject")
+//            .text("Hello world!")
+//            .build()
+//            .send();
     }
 
     @Override
@@ -72,22 +70,17 @@ public class ProdMailService implements MailService{
     @Override
     public void sendPasswordResetMail(User user) {
 
-        Email from = new Email("test@example.com");
-        Email to = new Email("tomkasp@gmail.com");
-        Content content = new Content("text/plain", "reset");
-        Mail mail = new Mail(from, "account reset", to, content);
-
-        Request request = new Request();
-        try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-            Response response = this.sendGrid.api(request);
-            sendGrid.api(request);
-
-        } catch (IOException ex) {
-            log.error("Error occured during email send", ex);
-        }
+        Client client = Client.create();
+        client.addFilter(new HTTPBasicAuthFilter("api", mailgunApiKey));
+        WebResource webResource = client.resource("https://api.mailgun.net/v3/" + domain
+            + "/messages");
+        MultivaluedMapImpl formData = new MultivaluedMapImpl();
+        formData.add("from", "Mailgun User <mailgun@" + domain + ">");
+        formData.add("to", "tomkasp@gmail.com");
+        formData.add("subject", "Simple Mailgun Example");
+        formData.add("text", "Plaintext content");
+        webResource.type(MediaType.APPLICATION_FORM_URLENCODED).post(ClientResponse.class,
+            formData);
     }
 
     @Override
