0fa5509b5d78d6a6d073d4fc3094f3db783a85b1
tomkasp@gmail.com <tomkasp@gmail.com>
2019-10-20 16:24:05 +0200

    tracker system url - handling manual activity scenario

--- tracker/src/test/java/com/athleticspot/tracker/domain/graph/SportActivityTest.java
+++ tracker/src/test/java/com/athleticspot/tracker/domain/graph/SportActivityTest.java
@@ -1,11 +1,14 @@
 package com.athleticspot.tracker.domain.graph;
 
 import com.athleticspot.tracker.domain.model.SportActivityBuilder;
+import com.athleticspot.tracker.domain.model.SportActivityType;
+import com.athleticspot.tracker.domain.model.TrackerSource;
 import javastrava.model.StravaActivity;
 import javastrava.model.reference.StravaActivityType;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 /**
@@ -18,9 +21,9 @@ public class SportActivityTest {
     private static final Long STRAVA_ID = 2734103741L;
 
     @Test
-    public void when_url_is_fetched_then_strava_url_is_generated(){
+    public void when_strava_activity_is_fetched_then_strava_url_is_generated() {
         //given
-        SportActivity sportActivity = createSportActivity();
+        SportActivity sportActivity = createStravaSportActivity();
 
         //when
         final String urlUnderTest = sportActivity.activityUrl();
@@ -29,7 +32,14 @@ public class SportActivityTest {
         Assertions.assertThat(urlUnderTest).isEqualToIgnoringCase(STRAVA_ACTIVITY_URL);
     }
 
-    private SportActivity createSportActivity() {
+    @Test
+    public void when_manual_activity_is_fetched_then_emtpy_string_is_returned() {
+
+        String urlUnderTest = createManualSportActivitySportActivity().activityUrl();
+        Assertions.assertThat(urlUnderTest).isEmpty();
+    }
+
+    private SportActivity createStravaSportActivity() {
         return SportActivityBuilder.createFromStravaActivity(
             createStravaActivity(),
             UUID.randomUUID().toString(),
@@ -45,4 +55,17 @@ public class SportActivityTest {
         return result;
     }
 
+    private SportActivity createManualSportActivitySportActivity() {
+        return new SportActivityBuilder(
+            TrackerSource.MANUAL,
+            SportActivityType.BACKCOUNTRY_SKI,
+            124L,
+            "",
+            11F,
+            LocalDateTime.now(),
+            "title",
+            UUID.randomUUID().toString(),
+            "Tom Kasp"
+        ).createSportActivity();
+    }
 }
